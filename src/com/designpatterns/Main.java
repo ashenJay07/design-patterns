2b5b18fc6d1c01043a72a633a9aa4b321ddefd8d
Ashen Jayarathne <ashenj.official@gmail.com>
2024-04-29 15:58:28 +0530

    Singleton problem

--- src/com/designpatterns/Main.java
+++ src/com/designpatterns/Main.java
@@ -1,7 +1,13 @@
 package com.designpatterns;
 
+import com.designpatterns.singleton.ConfigManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        ConfigManager manager = new ConfigManager();
+        manager.set("name", "Ashen");
+
+        ConfigManager other = new ConfigManager();
+        System.out.println(other.get("name"));
     }
 }
